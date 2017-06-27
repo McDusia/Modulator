390ca365160b74d07a8469d8744e75c9b05b722b
Magdalena Nowak <madzia.nowak222@poczta.fm>
2017-06-27 23:33:32 +0200

    all types is working - better code4

--- src/main/scala-2.11/scala/ChordBuilder.scala
+++ src/main/scala-2.11/scala/ChordBuilder.scala
@@ -295,6 +295,7 @@ object ChordBuilder {
   }
 
 
+
   def findIndexes(majorDestination: Boolean, array: Array[Int], findIfMajor: Array[Int], findIfMinor: Array[Int]) = {
     var indexes = new Array [Int] (4)
     val withoutBass = new Array [Int] (3)
@@ -310,41 +311,6 @@ object ChordBuilder {
     }
     indexes(0) = -1
     indexes.map(e=> (e+1))
-  }
-
-
-  //-----------------drugi typ
-  def thirdChord2type(majorDestination: Boolean, array: Array[Int]) = {
-    val indexes = findIndexes(majorDestination,array,Array(0,9,6,3),Array(0,9,6,3))
-    var map = Map(indexes(0)->0)
-    if(majorDestination)
-      map = Map(indexes(0) -> 1, indexes(1) -> 1,indexes(2) -> 0,indexes(3) -> -2)
-    else
-      map = Map(indexes(0) -> 1, indexes(1) -> 0,indexes(2) -> 0,indexes(3) -> -2)
-
-    moveNotes(array,map)
-  }
-
-  def fourthChord2Type(majorDestination: Boolean, array: Array[Int]) = {
-    val indexes = findIndexes(majorDestination,array,Array(0,9,12,5),Array(0,8,12,5))
-    var map = Map(indexes(0)->0)
-    if(majorDestination)
-      map = Map(indexes(0) -> 0, indexes(1) -> 1,indexes(2) -> 2,indexes(3) -> -1)
-    else
-      map = Map(indexes(0) -> 0, indexes(1) -> 2,indexes(2) -> 1,indexes(3) -> -1)
-
-    moveNotes(array,map)
-  }
-
-  def fifthChord2Type(majorDestination: Boolean, array: Array[Int]) = {
-    val indexes = findIndexes(majorDestination,array,Array(0,10,2,4),Array(0,10,1,4))
-    var map = Map(indexes(0)->0)
-    if(majorDestination)
-      map = Map(indexes(0) -> -7, indexes(1) -> -1,indexes(2) -> -2,indexes(3) -> 1)
-    else
-      map = Map(indexes(0) -> -7, indexes(1) -> -2,indexes(2) -> -1,indexes(3) -> 1)
-
-    moveNotes(array,map)
   }
 
 
@@ -676,33 +642,7 @@ object ChordBuilder {
       buildVector(array8(0),array8(1),array8(2),array8(3))
   }
 
-  def buildSequenceForSecondType(major: Boolean, majorDestination: Boolean, source: Int, destination: Int) = {
-    val boxForTonic = tonic(major,source)
-    val boxForFirstChord = modulationChord1(ModulationType.Second, source,destination,boxForTonic)
 
-    //musi znalezc od 6 od destination wsrdo 4 dzwiekow z define modula
-    //pozostale polaczyc najkrotsza droga  <- 1 akord po tonice
-
-    //przepisanie akordu do nowej tablicy
-    val tmp = new Array [Int] (4)
-    val tmp2 = new Array [Int] (4)
-    val tmp3 = new Array [Int] (4)
-    for(i <- 0 to 3) tmp(i) = boxForFirstChord(i)
-
-    val array3 = thirdChord2type(majorDestination,tmp)
-    //4 akord
-    for(i <- 0 to 3) tmp2(i) = array3(i)
-    val array4 = fourthChord2Type(majorDestination,tmp2)
-    for(i <- 0 to 3) tmp3(i) = array4(i)
-    val array5 = fifthChord2Type(majorDestination,tmp3)
-
-    buildVector(boxForTonic(0),boxForTonic(1),boxForTonic(2),boxForTonic(3)) ++
-      buildVector(boxForFirstChord(0),boxForFirstChord(1),boxForFirstChord(2),boxForFirstChord(3)) ++
-      buildVector(array3(0),array3(1),array3(2),array3(3)) ++
-      buildVector(array4(0),array4(1),array4(2),array4(3)) ++
-      buildVector(array5(0),array5(1),array5(2),array5(3))
-
-  }
 
   def buildSequenceForThirdType(major: Boolean, majorDestination: Boolean, source: Int, destination: Int) = {
     val boxForTonic = tonic(major,source)
